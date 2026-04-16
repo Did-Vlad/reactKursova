fd51f422d3123872b227fdb47b4da5908c96bf88
Vladyslav <ist23-v.didenko@nubip.edu.ua>
2026-04-16 09:26:00 +0300

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./Components/EmployeeDetails";
 const initialEmployee = [
 {id: 1, name:"Анна Ковальчук", position: "Fullstack Developer", salary: "55000", experience: "4 years", skills: ["React", "Laravel", "SQL"], img: "https://xsgames.co/randomusers/assets/avatars/male/1.jpg", description: "Відповідає за архітектуру бази даних."}, 
 {id: 1, name:"Вадим Коваль", position: "UI/UX Designer", salary: "40000", experience: "2 years", skills: ["Figma", "Photoshop"], img: "https://xsgames.co/randomusers/assets/avatars/male/1.jpg", description: "Створює сучасні інтерфейси."},
-{id: 1, name:"Олег Мороз", position: "Project Manager", salary: "60000", experience: "6 years", skills: ["Agile", "Jira"], img: "https://xsgames.co/randomusers/assets/avatars/male/1.jpg", description: "Керує термінами та командою."},
+{id: 1, name:"SІгорь Мороз", position: "Project Manager", salary: "60000", experience: "6 years", skills: ["Agile", "Jira"], img: "https://xsgames.co/randomusers/assets/avatars/male/1.jpg", description: "Керує термінами та командою."},
 ];
 
 
